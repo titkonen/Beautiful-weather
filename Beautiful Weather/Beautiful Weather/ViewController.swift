d29d8f894500541f5e4bf26157b69dc8d921b637
titkonen <toni.itkonen@icloud.com>
2019-02-27 23:02:04 +0200

    2019-2-27-session
    At the beginning of the dark sky section.

---
+++ Beautiful Weather/Beautiful Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Beautiful Weather
+//
+//  Created by Toni Itkonen on 2/27/19.
+//  Copyright © 2019 Toni Itkonen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Beautiful Weather/Beautiful WeatherTests/Beautiful_WeatherTests.swift
@@ -0,0 +1,34 @@
+//
+//  Beautiful_WeatherTests.swift
+//  Beautiful WeatherTests
+//
+//  Created by Toni Itkonen on 2/27/19.
+//  Copyright © 2019 Toni Itkonen. All rights reserved.
+//
+
+import XCTest
+@testable import Beautiful_Weather
+
+class Beautiful_WeatherTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
